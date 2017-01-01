dde9929146439e17a0e8a8833f75db2976c89497
Sean Kirmani <sean@kirmani.io>
2016-12-31 20:14:51 -0800

    removed resource listener from transtions

--- src/resource_controller.py
+++ src/resource_controller.py
@@ -23,6 +23,28 @@ kDebug = True
 kPlaces = ['requested_robot', 'free', 'owned_user', 'owned_robot',
            'requested_user']
 
+class BeliefControllerApi:
+    @staticmethod
+    def SetBelief(belief, value):
+        rospy.wait_for_service('set_belief')
+        try:
+            set_belief = rospy.ServiceProxy(
+                'set_belief', DoPetriNetArc)
+            set_belief(belief, value)
+        except rospy.ServiceException, e:
+            print("Service call failed: %s" % e)
+
+    @staticmethod
+    def GetBelief(belief):
+        rospy.wait_for_service('get_belief')
+        try:
+            get_belief = rospy.ServiceProxy(
+                'get_belief', DoPetriNetArc)
+            return get_belief(belief)
+        except rospy.ServiceException, e:
+            print("Service call failed: %s" % e)
+            return False
+
 class ResourceControllerApi:
     @staticmethod
     def RemoveResourceFromPlace(place, token):
@@ -82,143 +104,137 @@ class ResourceControllerApi:
             return False
 
 class ReleaseRobotTransition(PetriNetTransition):
-    def __init__(self, requested_robot, owned_robot, free, resource_listeners):
+    def __init__(self, requested_robot, owned_robot, free, beliefs):
         PetriNetTransition.__init__(self, 'release_robot')
         self.requested_robot_ = requested_robot
         self.owned_robot_ = owned_robot
         self.free_ = free
-        self.resource_listeners_ = resource_listeners
+        self.beliefs_ = beliefs
 
     def fire(self):
-        for resource_listener in self.resource_listeners_:
-            if (not self.requested_robot_.HasToken(resource_listener.name)
-                and self.owned_robot_.HasToken(resource_listener.name)):
+        for belief in self.beliefs_:
+            if (not self.requested_robot_.HasToken(belief)
+                and self.owned_robot_.HasToken(belief)):
                 if kVerbose:
                     print("Releasing resource (%s) from robot."
-                          % resource_listener.name)
-                self.owned_robot_.RemoveToken(resource_listener.name)
-                self.free_.AddToken(resource_listener.name)
+                          % belief)
+                self.owned_robot_.RemoveToken(belief)
+                self.free_.AddToken(belief)
 
     def activated(self):
-        for resource_listener in self.resource_listeners_:
-            if (not self.requested_robot_.HasToken(resource_listener.name)
-                and self.owned_robot_.HasToken(resource_listener.name)):
+        for belief in self.beliefs_:
+            if (not self.requested_robot_.HasToken(belief)
+                and self.owned_robot_.HasToken(belief)):
                 return True
         return False
 
 class YieldTransition(PetriNetTransition):
-    def __init__(self, requested_user, owned_robot, owned_user,
-                 resource_listeners):
+    def __init__(self, requested_user, owned_robot, owned_user, beliefs):
         PetriNetTransition.__init__(self, 'yield')
         self.requested_user_ = requested_user
         self.owned_robot_ = owned_robot
         self.owned_user_ = owned_user
-        self.resource_listeners_ = resource_listeners
+        self.beliefs_ = beliefs
 
     def fire(self):
-        for resource_listener in self.resource_listeners_:
-            if (self.requested_user_.HasToken(resource_listener.name)
-                and self.owned_robot_.HasToken(resource_listener.name)):
+        for belief in self.beliefs_:
+            if (self.requested_user_.HasToken(belief)
+                and self.owned_robot_.HasToken(belief)):
                 if kVerbose:
                     print("Yielding resource (%s) from robot to human."
-                          % resource_listener.name)
-                self.requested_user_.RemoveToken(resource_listener.name)
-                self.owned_robot_.RemoveToken(resource_listener.name)
-                self.owned_user_.AddToken(resource_listener.name)
+                          % belief)
+                self.requested_user_.RemoveToken(belief)
+                self.owned_robot_.RemoveToken(belief)
+                self.owned_user_.AddToken(belief)
 
     def activated(self):
-        for resource_listener in self.resource_listeners_:
-            if (self.requested_user_.HasToken(resource_listener.name)
-                and self.owned_robot_.HasToken(resource_listener.name)):
+        for belief in self.beliefs_:
+            if (self.requested_user_.HasToken(belief)
+                and self.owned_robot_.HasToken(belief)):
                 return True
         return False
 
 class RequestUserTransition(PetriNetTransition):
-    def __init__(self, owned_user, requested_user, resource_listeners, actions):
+    def __init__(self, owned_user, requested_user, beliefs):
         PetriNetTransition.__init__(self, 'request_user')
         self.owned_user_ = owned_user
         self.requested_user_ = requested_user
-        self.resource_listeners_ = resource_listeners
-        self.actions_ = actions
+        self.beliefs_ = beliefs
 
     def fire(self):
         # Place resource tokens in requested place.
-        for resource_listener in self.resource_listeners_:
-            if (not resource_listener.Poll(self.actions_)
-                and not self.requested_user_.HasToken(resource_listener.name)):
+        for belief in self.beliefs_:
+            if (not self.beliefs_[belief]
+                and not self.requested_user_.HasToken(belief)):
                 if kVerbose:
                     print("Requesting resource for user: %s"
-                          % resource_listener.name)
-                self.requested_user_.AddToken(resource_listener.name)
+                          % belief)
+                self.requested_user_.AddToken(belief)
 
     def activated(self):
-        for resource_listener in self.resource_listeners_:
-            if (not resource_listener.Poll(self.actions_)
-                and not self.requested_user_.HasToken(resource_listener.name)):
+        for belief in self.beliefs_:
+            if (not self.beliefs_[belief]
+                and not self.requested_user_.HasToken(belief)):
                 return True
         return False
 
 class SeizeUserTransition(PetriNetTransition):
-    def __init__(self, requested_user, free, owned_user, resource_listeners):
+    def __init__(self, requested_user, free, owned_user, beliefs):
         PetriNetTransition.__init__(self, 'seize_user')
         self.requested_user_ = requested_user
         self.free_ = free
         self.owned_user_ = owned_user
-        self.resource_listeners_ = resource_listeners
+        self.beliefs_ = beliefs
 
     def fire(self):
-        for resource_listener in self.resource_listeners_:
-            if (self.requested_user_.HasToken(resource_listener.name)
-                and self.free_.HasToken(resource_listener.name)):
+        for belief in self.beliefs_:
+            if (self.requested_user_.HasToken(belief)
+                and self.free_.HasToken(belief)):
                 if kVerbose:
                     print("Seizing resource for user: %s"
-                          % resource_listener.name)
-                self.free_.RemoveToken(resource_listener.name)
-                self.owned_user_.AddToken(resource_listener.name)
+                          % belief)
+                self.free_.RemoveToken(belief)
+                self.owned_user_.AddToken(belief)
 
     def activated(self):
-        for resource_listener in self.resource_listeners_:
-            if (self.requested_user_.HasToken(resource_listener.name)
-                and self.free_.HasToken(resource_listener.name)):
+        for belief in self.beliefs_:
+            if (self.requested_user_.HasToken(belief)
+                and self.free_.HasToken(belief)):
                 return True
         return False
 
 class ReleaseUserTransition(PetriNetTransition):
-    def __init__(self, requested_user, owned_user, free, resource_listeners,
-                 actions):
+    def __init__(self, requested_user, owned_user, free, beliefs):
         PetriNetTransition.__init__(self, 'release_user')
         self.requested_user_ = requested_user
         self.owned_user_ = owned_user
         self.free_ = free
-        self.resource_listeners_ = resource_listeners
-        self.actions_ = actions
+        self.beliefs_ = beliefs
 
     def fire(self):
-        for resource_listener in self.resource_listeners_:
-            if (resource_listener.Poll(self.actions_)
-                and self.owned_user_.HasToken(resource_listener.name)):
+        for belief in self.beliefs_:
+            if (self.beliefs_[belief]
+                and self.owned_user_.HasToken(belief)):
                 if kVerbose:
                     print("Releasing resource for user: %s"
-                          % resource_listener.name)
-                self.requested_user_.RemoveToken(resource_listener.name)
-                self.owned_user_.RemoveToken(resource_listener.name)
-                self.free_.AddToken(resource_listener.name)
+                          % belief)
+                self.requested_user_.RemoveToken(belief)
+                self.owned_user_.RemoveToken(belief)
+                self.free_.AddToken(belief)
 
     def activated(self):
-        for resource_listener in self.resource_listeners_:
-            if (resource_listener.Poll(self.actions_)
-                and self.owned_user_.HasToken(resource_listener.name)):
+        for belief in self.beliefs_:
+            if (self.beliefs_[belief]
+                and self.owned_user_.HasToken(belief)):
                 return True
         return False
 
 class ResourceController(PetriNet):
-    def __init__(self, resource_listeners):
+    def __init__(self):
         PetriNet.__init__(self, 'resource_controller')
         self.places_ = {}
         self.actions_ = Set()
         self.beliefs_ = {}
-
-        self.resource_listeners_ = resource_listeners
 
         # Places.
         for place in kPlaces:
@@ -229,33 +245,33 @@ class ResourceController(PetriNet):
             ReleaseRobotTransition(self.places_['requested_robot'],
                                    self.places_['owned_robot'],
                                    self.places_['free'],
-                                   self.resource_listeners_))
+                                   self.beliefs_))
         self.transitions_.append(
             YieldTransition(self.places_['requested_user'],
                             self.places_['owned_robot'],
                             self.places_['owned_user'],
-                            self.resource_listeners_))
+                            self.beliefs_))
         self.transitions_.append(
             RequestUserTransition(self.places_['owned_user'],
                                   self.places_['requested_user'],
-                                  self.resource_listeners_,
-                                  self.actions_))
+                                  self.beliefs_))
         self.transitions_.append(
             SeizeUserTransition(self.places_['requested_user'],
                                 self.places_['free'],
                                 self.places_['owned_user'],
-                                self.resource_listeners_))
+                                self.beliefs_))
         self.transitions_.append(
             ReleaseUserTransition(self.places_['requested_user'],
                                   self.places_['owned_user'],
                                   self.places_['free'],
-                                  self.resource_listeners_,
-                                  self.actions_))
-
-        for resource_listener in self.resource_listeners_:
-            self.places_['free'].AddToken(resource_listener.name)
+                                  self.beliefs_))
 
     def SetBelief(self, belief, value):
+        if belief not in self.beliefs_:
+            if value:
+                self.places_['free'].AddToken(belief)
+            else:
+                self.places_['owned_user'].AddToken(belief)
         self.beliefs_[belief] = value
         print("SetBelief: %s -> %s" % (belief, value))
         print(self.beliefs_)
@@ -301,6 +317,7 @@ class ResourceController(PetriNet):
         if kDebug:
             print("Adding active action: %s" % action)
         self.actions_.add(action)
+        # self.SetBelief('action_active_' + action, True)
 
     def RemoveActiveAction(self, action):
         if kDebug:
@@ -308,6 +325,7 @@ class ResourceController(PetriNet):
         if action not in self.actions_:
             return False
         self.actions_.remove(action)
+        # self.SetBelief('action_active_' + action, True)
         return True
 
 def handle_do_petri_net_arc(req):
@@ -341,12 +359,7 @@ def OnShutdown():
 def main():
     global resource_controller
 
-    # Add resource listeners.
-    resource_listeners = Set()
-    # floor_listener = FloorListener()
-    # resource_listeners.add(floor_listener)
-
-    resource_controller = ResourceController(resource_listeners)
+    resource_controller = ResourceController()
     if kDebug:
         print("Initial marking: %s" % str(resource_controller.GetMarking()))
 
